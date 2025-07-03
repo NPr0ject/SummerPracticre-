f42aa72c74b7a5e15c6e33b50976d570a900743e
NPr0ject <iwanow32855@gmail.com>
2025-07-03 19:39:18 +0600

    Created a solution for task10

---
+++ Plugin2/Class1.cs
@@ -0,0 +1,11 @@
+﻿using PluginHost;
+using Plugin1;
+
+namespace Plugin2;
+
+[PluginLoad]
+[DependsOn(typeof(plugin1))]
+public class plugin2
+{
+    public void Execute() => Console.WriteLine("2");
+}
---
+++ PluginHost/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection;
+
+namespace PluginHost;
+
+public class PluginLoadAttribute : Attribute {}
+
+public class DependsOnAttribute : Attribute
+{
+    public Type Dependency { get; }
+    public DependsOnAttribute(Type dependency)
+    {
+        Dependency = dependency;
+    }
+}
+
+public class PluginLoader
+{
+    private Dictionary<Type, List<Type>> dependencyGraph = new();
+    private List<Type> TopologicalSort(Dictionary<Type, List<Type>> graph)
+    {
+        var result = new List<Type>();
+        var visited = new HashSet<Type>();
+        var visiting = new HashSet<Type>();
+
+        void Visit(Type node)
+        {
+            if (visited.Contains(node)) return;
+            if (visiting.Contains(node))
+                throw new Exception($"asd");
+
+            visiting.Add(node);
+
+            if (graph.TryGetValue(node, out var dependencies))
+            {
+                foreach (var dep in dependencies)
+                {
+                    Visit(dep);
+                }
+            }
+
+            visiting.Remove(node);
+            visited.Add(node);
+            result.Add(node);
+        }
+
+        foreach (var node in graph.Keys)
+        {
+            Visit(node);
+        }
+
+        return result;
+    }
+    
+    public void LoadPlugins(string folderPath)
+    {
+        var dllFiles = Directory.GetFiles(folderPath, "*.dll");
+        var assemblies = dllFiles.Select(Assembly.LoadFrom).ToList();
+
+        foreach (var asm in assemblies)
+        {
+            foreach (var type in asm.GetTypes())
+            {
+                if (type.GetCustomAttribute<PluginLoadAttribute>() != null)
+                {
+                    var dependencies = type.GetCustomAttributes<DependsOnAttribute>()
+                                           .Select(d => d.Dependency)
+                                           .ToList();
+
+                    dependencyGraph[type] = dependencies;
+                }
+            }
+        }
+
+        var sortedPlugins = TopologicalSort(dependencyGraph);
+
+        foreach (var pluginType in sortedPlugins)
+        {
+            var instance = Activator.CreateInstance(pluginType);
+            var executeMethod = pluginType.GetMethod("Execute", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            executeMethod?.Invoke(instance, null);
+        }
+    }
+}
