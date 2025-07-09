c43bf0ed0c5efad77cfe927b0de4cf36d743b96c
NPr0ject <iwanow32855@gmail.com>
2025-07-09 20:59:45 +0600

    Created a solution for task13

---
+++ task13/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace task13;
+
+public class Subject
+{
+  public string? Name {get; set; }
+  public int Grade {get; set; }
+}
+
+public class Student
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<Subject>? Grades { get; set; }
+}
+
+public class SerializerDeserializer()
+{
+    private readonly JsonSerializerOptions options = new()
+    {
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonDateConverter("dd.MM.yyyy")}
+    };
+    public void SerializerSafeFile(Student student, string path)
+    {
+        string json = JsonSerializer.Serialize(student);
+        File.WriteAllText(path, json);
+    }
+    public Student DeserializerLoadFile(string path)
+    {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<Student>(json)!;
+    }
+    
+public class JsonDateConverter : JsonConverter<DateTime>
+{
+    private readonly string _format;
+
+    public JsonDateConverter(string format)
+    {
+        _format = format;
+    }
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var s = reader.GetString()!;
+        return DateTime.ParseExact(s, _format, null);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(_format));
+    }
+}
+}
